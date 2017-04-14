d3bc817e05546e4de416c4145fd1148603337ec1
Tyler Vermillion <mtvextreme1998@yahoo.com>
2017-04-14 10:26:03 -0400

    Added WebApi for the Star Toggle

--- ElevenNote.Models/NoteDetail.cs
+++ ElevenNote.Models/NoteDetail.cs
@@ -16,6 +16,7 @@ namespace ElevenNote.Models
 
         public string Content { get; set; }
 
+        public bool IsStarred { get; set; }
 
         [Display(Name = "Created")]
         public DateTimeOffset CreatedUtc { get; set; }
---
+++ ElevenNote.Web/Controllers/WebAPI/NoteController.cs
@@ -0,0 +1,49 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.Web.Controllers.WebAPI
+{
+    [Authorize]
+    [RoutePrefix("api/Note")]
+    public class NoteController : ApiController
+    {
+        private bool SetStarState(int noteId, bool newState)
+        {
+            //Create Serivce
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+
+            var detail = service.GetNoteById(noteId);
+            //Create NoteEdit model instance with new start state
+            var updatedNote =
+                new NoteEdit
+                {
+                    NoteId = detail.NoteId,
+                    Title = detail.Title,
+                    Content = detail.Content
+                    //TODO: 
+                };
+
+            //Return Value indicating whether the updated succeeded
+            return service.UpdateNote(updatedNote);
+        }
+
+        [Route("{id}/Star")]
+        [HttpPut]
+        public bool ToggleStarOn(int id) => SetStarState(id, true);
+
+
+
+        [Route("{id}/Star")]
+        [HttpDelete]
+        public bool ToggleStarOff(int id) => SetStarState(id, false);
+
+    }
+}
