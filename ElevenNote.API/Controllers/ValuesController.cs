74ae0796cc2446713ab4323b955e572ab9e7260f
Tyler Vermillion <mtvextreme1998@yahoo.com>
2017-04-24 16:52:54 -0400

    Added a Test

--- ElevenNote.API/Controllers/ValuesController.cs
+++ ElevenNote.API/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevenNote.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,9 +12,11 @@ namespace ElevenNote.API.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public IHttpActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            var service = new NoteService(Guid.Parse("6b0f8da3-e01e-437d-815b-a305c06fe8b6"));
+            var data = service.GetNotes();
+            return Ok(data);
         }
 
         // GET api/values/5
