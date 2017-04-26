1ebf2ce980d1c2f3262ec8050b5b08011aa892ad
Tyler Vermillion <mtvextreme1998@yahoo.com>
2017-04-26 17:07:57 -0400

    Added Login Code

--- ElevenNote.MobileApp/ElevenNote.MobileApp/App.xaml.cs
+++ ElevenNote.MobileApp/ElevenNote.MobileApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevenNote.MobileApp.ExternalServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,15 @@ namespace ElevenNote.MobileApp
 {
 	public partial class App : Application
 	{
+        /// <summary>
+        ///  Web Service Access
+        /// </summary>
+        internal static readonly NoteService NoteService = new NoteService();
 		public App ()
 		{
 			InitializeComponent();
 
-            this.MainPage = new NavigationPage(new NoteDetailPage());
+            this.MainPage = new NavigationPage(new LoginPage());
 		}
 
 		protected override void OnStart ()
