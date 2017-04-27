39c15c4bff1dbc78897d2b94c93d2b8d2498fb12
Tyler Vermillion <mtvextreme1998@yahoo.com>
2017-04-27 12:37:41 -0400

    Added Note View List code

--- ElevenNote.MobileApp/ElevenNote.MobileApp/NotesPage.xaml.cs
+++ ElevenNote.MobileApp/ElevenNote.MobileApp/NotesPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElevenNote.MobileApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,60 @@ namespace ElevenNote.MobileApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class NotesPage : ContentPage
 	{
+        private List<NoteListItemViewModel> Notes { get; set; } 
+
 		public NotesPage ()
 		{
 			InitializeComponent ();
+            SetupUi();
 		}
-	}
+
+        /// <summary>
+        /// Sets up user Interface without making designer confused
+        /// </summary>
+
+        private void SetupUi()
+        {
+            lvwNotes.IsPullToRefreshEnabled = true;
+            lvwNotes.Refreshing += async (o, args) =>
+            {
+                await PopulateNotesList();
+                lvwNotes.IsRefreshing = false;
+                lblNoNotes.IsVisible = !Notes.Any();
+            };
+        }
+
+        /// <summary>
+        /// Updates the note list view
+        /// </summary>
+        /// <returns></returns>
+        private async Task PopulateNotesList()
+        {
+            await App.NoteService.GetAll().ContinueWith(task =>
+            {
+                var notes = task.Result;
+
+                Notes = notes
+                        .OrderByDescending(note => note.IsStarred)
+                        .ThenByDescending(note => note.CreatedUtc)
+                        .Select(s => new NoteListItemViewModel
+                        {
+                            NoteId = s.NoteId,
+                            Title = s.Title,
+                            StarImage = s.IsStarred ? "starred.png" : "notstarred.png"
+                        })
+                        .ToList();
+
+                lvwNotes.ItemsSource = Notes;
+
+                //CLear Item Slecteion
+                lvwNotes.SelectedItem = null;
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        protected override async void OnAppearing()
+        {
+            await PopulateNotesList();
+        }
+    }
 }
